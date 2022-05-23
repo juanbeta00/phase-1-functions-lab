446ef4bb13d44a5056ba2fc073800303b5b7b5a4
Juan <juanbeta00@hotmail.com>
2022-05-23 10:13:29 -0400

    commit tree c8b0e6bbec4f1029ef8f3081acb680e6d622f808

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLoc) {
+    // pickUpLoc = Math.abs(pickUpLoc);
+    let distance = (42 - pickUpLoc);
+    return Math.abs(distance);
+}
+
+function distanceFromHqInFeet(blocks) {
+    let distInFeet = (distanceFromHqInBlocks(blocks) * 264)
+    return distInFeet;
+
+}
+
+function distanceTravelledInFeet(start, end) {
+    let travInFeet = ((start - end) * 264)
+    return Math.abs(travInFeet);
+}
+// function calculatesFarePrice(start, end) {
+//     let distInFeet = ((start - end) * 264)
+//     let charge = ''
+//     if (distInFeet > 2500) {
+//         charge = 'cannot travel that far';
+//     } else if (distInFeet > 2000) {
+//         charge = 25;     
+//     } else if (distInFeet > 400) {
+//         charge = ((distInFeet - 400) * 0.02);
+//     } else {
+//         charge = 0;
+//     }
+//     return charge;
+// }
+
+function calculatesFarePrice(start, end) {
+    let dstnce = distanceTravelledInFeet(start, end)
+    if (dstnce < 400) {
+        return 0;
+    } else if (dstnce <= 2000) {
+        return (dstnce - 400) * 0.02;
+    } else if (dstnce <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
